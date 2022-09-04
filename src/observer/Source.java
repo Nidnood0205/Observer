c2729b5f43fb2d1c3b520b42159e079271150235
Nidnood0205 <kow3415@gmail.com>
2022-09-04 22:51:47 +0700

    ex3.3

---
+++ src/observer/Source.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer;
+
+/**
+ *
+ * @author DELL
+ */
+public interface Source {
+    public void notifyObservers();
+    public void create(MyObserver observer);
+}
